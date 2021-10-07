ce180ca58bc5a6c3987a6ee11978c73864945045
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-07 17:50:06 -0400

    create cloud gateway app

--- CloudGateway/src/main/java/service/CloudgatewayApplication.java
+++ CloudGateway/src/main/java/service/CloudgatewayApplication.java
@@ -3,6 +3,9 @@ package service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,20 @@ public class CloudgatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/student/**")
+//								.filters(f -> f.addRequestHeader("Hello", "World"))
+								.uri("lb://StudentService")
+//                        .id("studentModule")
+				)
+				.route(r -> r.path("/grade/**")
+								.uri("lb://GradingService")
+				)
+				.build();
 	}
 
 }
